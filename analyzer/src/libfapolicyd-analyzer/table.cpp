ef087f4b64d0f840cd0979ff9831f8b5e5a8c04e
Davin Shearer <scholarsmate@gmail.com>
2020-07-14 12:00:59 -0400

    simplify table impl

--- analyzer/src/libfapolicyd-analyzer/table.cpp
+++ analyzer/src/libfapolicyd-analyzer/table.cpp
@@ -1,15 +1,12 @@
 #include "table.hpp"
 #include "error_codes.h"
-#include <algorithm>
 #include <boost/numeric/conversion/cast.hpp>
+#include <utility>
 
-typedef struct column_struct {
-  std::string name;
-  vect_of_strings_t column;
-} column_t;
-
+typedef std::pair<std::string, vect_of_strings_t> column_t;
+typedef std::vector<column_t> columns_t;
 struct table_t::Impl_t {
-  std::vector<column_t> columns;
+  columns_t columns;
 };
 
 table_t::table_t() : pimpl_(new Impl_t) {}
@@ -21,8 +18,7 @@ int table_t::initialize(const vect_of_strings_t &column_names) {
   if (0 < column_count) {
     pimpl_->columns.reserve(column_count);
     for (int col_index = 0; col_index < column_count; ++col_index) {
-      pimpl_->columns.push_back(column_t());
-      pimpl_->columns.back().name = column_names[col_index];
+      pimpl_->columns.push_back(std::make_pair(column_names[col_index], vect_of_strings_t()));
     }
     return OK;
   }
@@ -40,7 +36,7 @@ int table_t::get_column_count() const {
 int table_t::get_row_count() const {
   if (0 < get_column_count()) {
     try {
-      return boost::numeric_cast<int>(pimpl_->columns[0].column.size());
+      return boost::numeric_cast<int>(pimpl_->columns[0].second.size());
     } catch (std::exception &) {
       return RANGE_ERROR;
     }
@@ -48,11 +44,11 @@ int table_t::get_row_count() const {
   return NOT_FOUND;
 }
 
-std::string table_t::get_column_name(int col_num) const { return pimpl_->columns[col_num].name; }
+std::string table_t::get_column_name(int col_num) const { return pimpl_->columns[col_num].first; }
 
 int table_t::find_column_number(const std::string &column_name) const {
-  for (std::vector<column_t>::const_iterator iter(pimpl_->columns.cbegin()); iter != pimpl_->columns.cend(); ++iter) {
-    if (column_name == iter->name) {
+  for (columns_t::const_iterator iter(pimpl_->columns.cbegin()); iter != pimpl_->columns.cend(); ++iter) {
+    if (column_name == iter->first) {
       try {
         return boost::numeric_cast<int>(std::distance(pimpl_->columns.cbegin(), iter));
       } catch (std::exception &) {
@@ -63,25 +59,23 @@ int table_t::find_column_number(const std::string &column_name) const {
   return NOT_FOUND;
 }
 
-std::string table_t::get_value(int col_num, int row_num) const { return pimpl_->columns[col_num].column[row_num]; }
+std::string table_t::get_value(int col_num, int row_num) const { return pimpl_->columns[col_num].second[row_num]; }
 
 int table_t::push_back_row(const vect_of_strings_t &row_values) {
   const int column_count = get_column_count();
-  if (column_count == row_values.size()) {
-    for (int col_index = 0; col_index < column_count; ++col_index) {
-      pimpl_->columns[col_index].column.push_back(row_values[col_index]);
-    }
-    return OK;
+  if (column_count != row_values.size()) {
+    return RANGE_ERROR;
   }
-  return RANGE_ERROR;
+  for (int col_index = 0; col_index < column_count; ++col_index) {
+    pimpl_->columns[col_index].second.push_back(row_values[col_index]);
+  }
+  return OK;
 }
 
 int table_t::push_back_column(const std::string &column_name, const vect_of_strings_t &column_values) {
-  if (get_row_count() == column_values.size()) {
-      pimpl_->columns.push_back(column_t());
-      pimpl_->columns.back().name = column_name;
-      pimpl_->columns.back().column = column_values;
-    return OK;
+  if (get_row_count() != column_values.size()) {
+    return RANGE_ERROR;
   }
-  return RANGE_ERROR;
+  pimpl_->columns.push_back(std::make_pair(column_name, column_values));
+  return OK;
 }
